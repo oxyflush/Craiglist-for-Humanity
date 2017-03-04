52e482d5a3e94310964709318ef5439c622f356d
oxyflush <dan.zhan10@gmail.com>
2017-03-04 12:59:16 -0800

    Add files via upload

---
+++ submit.js
@@ -0,0 +1,8 @@
+<input type="button" class="btn btn-info" value="Input Button" onclick="  relocate()">
+
+<script>
+function relocate_home()
+{
+     location.href = "individual.html";
+} 
+</script>
